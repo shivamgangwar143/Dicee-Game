68806b948e14101a90dbc8abd574279c5f6dabd6
Pragati Gupta <70162944+pragatiatgit2529@users.noreply.github.com>
2021-06-25 23:48:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1=Math.floor(Math.random()*6+1);
 var randomImage="dice"+randomNumber1+".png";
-var randomImageSource="images/"+randomImage;
+var randomImageSource=randomImage;
 document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
 var randomNumber2=Math.floor(Math.random()*6+1);
 var randomImage2="dice"+randomNumber2+".png";
-var randomImageSource2="images/"+randomImage2;
+var randomImageSource2=randomImage2;
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 if (randomNumber1>randomNumber2){
   var result=document.querySelector("h1").innerHTML="🎉Player-1 wins";
